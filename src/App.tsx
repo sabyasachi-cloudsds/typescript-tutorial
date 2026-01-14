725fc190460c893356cbccda7c692e3a91a3c49e
sabyasachi-cloudsds <sabyasachi.rakshit@cloudsds.com>
2026-01-14 11:48:29 +0530

    learnign types in typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+let name: string;
+let age: number | string;
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string];
+let printName: (name: string) => never;
+
+let personName: unknown;
+
+// type Person={
+//   name:string;
+//   age?:number;
+// }
+
+// let person : Person = {
+//   name:"SR"
+// };
+
+// let lotsOfPeople:Person[]
+
+interface Person {
+  name: string;
+  age?: number;
+}
+
+interface Guy extends Person {
+  profession?: string;
+  salary?: number;
+}
+
+let sr: Guy = {
+  name: "SR",
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">Hello world</div>;
 }
 
 export default App;
